307a033fdcdafb125bd347599b7759754759fb72
mvluz <marcus.i.t@outlook.com>
2021-08-03 22:22:35 -0300

    Created proj and Classes Import and ImportItem

---
+++ ImportExcelDataAPI/ImportExcelDataAPI/Models/Import.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ImportExcelDataAPI.Models
+{
+    public class Import
+    {
+        [Key]
+        [Display(Name = "ID do Item")]
+        public int ImportID { get; set; }
+
+        [Display(Name = "Data e Hora da Importação")]
+        public DateTime CreatedON { get; set; }
+
+        [Display(Name = "Local do Arquivo")]
+        public string FileLocal { get; set; }
+        public ICollection<ImportItem> ItemsList { get; set; } = new List<ImportItem>();
+
+        public Import()
+        {
+        }
+
+        public Import(int importID, DateTime createdON, string fileLocal)
+        {
+            ImportID = importID;
+            CreatedON = createdON;
+            FileLocal = fileLocal;
+        }
+
+        public void AddItems(ImportItem Item)
+        {
+            ItemsList.Add(Item);
+        }
+
+        public double TotalItemListAmount()
+        {
+            return ItemsList.Sum(Item => Item.Amount);
+        }
+
+        public double TotalItemListUnitayValue()
+        {
+            return ItemsList.Sum(item => item.Amount * item.UnitaryValue);
+        }
+    }
+}
---
+++ ImportExcelDataAPI/ImportExcelDataAPI/Models/ImportItem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ImportExcelDataAPI.Models
+{
+    public class ImportItem
+    {
+        [Key]
+        [Display(Name = "ID do Item")]
+        public int ImportItemID { get; set; }
+
+        [Required(ErrorMessage = "{0} é uma informação obrigatória")]
+        [Display(Name = "Data de Entrega")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime DeliveryDate { get; set; }
+
+        [Required(ErrorMessage = "{0} é uma informação obrigatória")]
+        [Display(Name = "Nome do Produto")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "{0} deve ter no máximo {1} caracteres")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "{0} é uma informação obrigatória")]
+        [Display(Name = "Quantidade")]
+        public int Amount { get; set; }
+
+        [Required(ErrorMessage = "{0} é uma informação obrigatória")]
+        [Display(Name = "Valor Unitário")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
+        public double UnitaryValue { get; set; }
+
+        public Import Import { get; set; }
+
+        [Required]
+        public int ImportID { get; set; }
+
+        public ImportItem()
+        {
+        }
+
+        public ImportItem(int importItemID, DateTime deliveryDate, string productName, int amount, double unitaryValue, Import import, int importID)
+        {
+            ImportItemID = importItemID;
+            DeliveryDate = deliveryDate;
+            ProductName = productName;
+            Amount = amount;
+            UnitaryValue = unitaryValue;
+            Import = import;
+            ImportID = importID;
+        }
+
+        public double SubTotal(ImportItem item)
+        {
+            return item.Amount + item.UnitaryValue;
+        }
+
+    }
+}
