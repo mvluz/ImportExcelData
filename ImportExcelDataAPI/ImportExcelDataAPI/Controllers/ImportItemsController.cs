ea3e27c4f77580522a11c5fc55e0a1898e7d55fe
mvluz <marcus.i.t@outlook.com>
2021-08-04 08:40:35 -0300

    Created Controllers with scaffold

---
+++ ImportExcelDataAPI/ImportExcelDataAPI/Controllers/ImportItemsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ImportExcelDataAPI.Models;
+
+namespace ImportExcelDataAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImportItemsController : ControllerBase
+    {
+        private readonly APIContext _context;
+
+        public ImportItemsController(APIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ImportItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ImportItem>>> GetImportItems()
+        {
+            return await _context.ImportItems.ToListAsync();
+        }
+
+        // GET: api/ImportItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ImportItem>> GetImportItem(int id)
+        {
+            var importItem = await _context.ImportItems.FindAsync(id);
+
+            if (importItem == null)
+            {
+                return NotFound();
+            }
+
+            return importItem;
+        }
+
+        // PUT: api/ImportItems/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutImportItem(int id, ImportItem importItem)
+        {
+            if (id != importItem.ImportItemID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(importItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ImportItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ImportItems
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ImportItem>> PostImportItem(ImportItem importItem)
+        {
+            _context.ImportItems.Add(importItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetImportItem", new { id = importItem.ImportItemID }, importItem);
+        }
+
+        // DELETE: api/ImportItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteImportItem(int id)
+        {
+            var importItem = await _context.ImportItems.FindAsync(id);
+            if (importItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.ImportItems.Remove(importItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ImportItemExists(int id)
+        {
+            return _context.ImportItems.Any(e => e.ImportItemID == id);
+        }
+    }
+}
---
+++ ImportExcelDataAPI/ImportExcelDataAPI/Controllers/ImportsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ImportExcelDataAPI.Models;
+
+namespace ImportExcelDataAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImportsController : ControllerBase
+    {
+        private readonly APIContext _context;
+
+        public ImportsController(APIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Imports
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Import>>> GetImports()
+        {
+            return await _context.Imports.ToListAsync();
+        }
+
+        // GET: api/Imports/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Import>> GetImport(int id)
+        {
+            var import = await _context.Imports.FindAsync(id);
+
+            if (import == null)
+            {
+                return NotFound();
+            }
+
+            return import;
+        }
+
+        // PUT: api/Imports/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutImport(int id, Import import)
+        {
+            if (id != import.ImportID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(import).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ImportExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Imports
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Import>> PostImport(Import import)
+        {
+            _context.Imports.Add(import);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetImport", new { id = import.ImportID }, import);
+        }
+
+        // DELETE: api/Imports/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteImport(int id)
+        {
+            var import = await _context.Imports.FindAsync(id);
+            if (import == null)
+            {
+                return NotFound();
+            }
+
+            _context.Imports.Remove(import);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ImportExists(int id)
+        {
+            return _context.Imports.Any(e => e.ImportID == id);
+        }
+    }
+}
